bf7942e8e34b5b90506f1940eabbed1a891831b3
jinxx45 <www.pragat7@pec.edu>
2021-07-10 23:44:25 +0530

    initial commit with chartJS

---
+++ src/Pages/LandingPage/LandingPage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import VerticalBar from '../../chartTypes/VerticalBar'
+
+
+function LandingPage() {
+    return (
+        <div>
+            <VerticalBar/>
+        </div>
+    )
+}
+
+export default LandingPage
